147c2ebf7051d00c127372e1e9c98ec2db3a5a2a
Bwvdhelm <bvanderh@gmail.com>
2019-02-12 20:07:47 +0100

    updated calc.py

--- neural_network_from_scratch/calculation.py
+++ neural_network_from_scratch/calculation.py
@@ -7,7 +7,7 @@ def setup():
 
     training_data = [[[0, 0], [0]], [[1, 1], [0]], [[1, 0], [1]], [[0, 1], [1]]]
 
-    for i in range(50000):
+    for i in range(100000):
         item = random.choice(training_data)
         nn.train(item[0], item[1])
 
