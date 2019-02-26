892c1ed5d7f3b6a6d3666364397ba28a5baa9064
Bwvdhelm <bvanderh@gmail.com>
2019-02-26 15:13:06 +0100

    Updated calculations on nn from scatch

--- neural_network_from_scratch/calculation.py
+++ neural_network_from_scratch/calculation.py
@@ -7,6 +7,9 @@ def setup():
 
     training_data = [[[0, 0], [0]], [[1, 1], [0]], [[1, 0], [1]], [[0, 1], [1]]]
 
+    X = [[0, 0], [1, 1], [0, 1], [1, 0]]
+    y = [[0], [0], [1], [1]]
+
     for i in range(100000):
         item = random.choice(training_data)
         nn.train(item[0], item[1])
