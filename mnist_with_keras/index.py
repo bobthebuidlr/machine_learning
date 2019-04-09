7bbbcf6624340e50f40e395b187b84e448d4f141
Bwvdhelm <bvanderh@gmail.com>
2019-04-09 13:51:23 +0200

    Updated

--- mnist_with_keras/index.py
+++ mnist_with_keras/index.py
@@ -1,26 +1,64 @@
 import tensorflow as tf
 import numpy as np
-import pandas as pd
-
 import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 
-with np.load('data/mnist.npz') as d:
+EPOCHS = 5
 
+
+def show_image(img):
+    plt.imshow(img, cmap="Dark2")
+    plt.show()
+
+
+def define_model():
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=x_test[0].shape),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+
+    return model
+
+
+def train_model(x_train, y_train, x_test, y_test):
+    model = define_model()
+
+    model.compile(
+        optimizer='adam',
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    model.fit(x=x_train, y=y_train, epochs=EPOCHS)
+
+    model.save('models/3_layer.h5')
+
+    (loss, acc) = model.evaluate(x_test, y_test)
+
+    print('loss: ', loss, '\n', 'accuracy: ', acc)
+
+
+def load_model():
+    model.summary()
+    # return model.load_weights('models/3_layer.h5')
+
+
+with np.load('data/mnist.npz') as d:
     x_train = d['x_train']
     y_train = d['y_train']
     x_test = d['x_test']
     y_test = d['y_test']
 
-    x_train = tf.keras.utils.normalize(x_train)
-    y_train = tf.keras.utils.normalize(y_train)
+    x_train = tf.keras.utils.normalize(x_train, axis=1)
+    x_test = tf.keras.utils.normalize(x_test, axis=1)
 
-    plt.imshow(x_train[0], cmap = plt.cm.binary)
-    plt.show()
+    # train_model(x_train, y_train, x_test, y_test)
+    model = tf.keras.models.load_model('models/3_layer.h5')
+    model.summary()
+    example = x_test[0].reshape(1, 28, 28)
+    model.predict(example)
 
-
-def show_image(img):
-    plt.imshow(img, cmap=plt.cm.binary)
-    plt.show()
 
